e8d806d66733bb8bc3f1efea9d10c31cda0f492a
Leslie Swan <42787368+PersonMeetup@users.noreply.github.com>
2022-02-23 13:14:22 -0700

    portfolio adjustments, create competitive page

---
+++ src/_data/tf2.js
@@ -0,0 +1,14 @@
+module.exports = function () {
+  return {
+    totalPlaytime: 2951.5,
+    scoutPlaytime: 312,
+    soldierPlaytime: 112.1,
+    pyroPlaytime: 47.7,
+    demomanPlaytime: 52,
+    heavyweaponsPlaytime: 44.4,
+    engineerPlaytime: 397.4,
+    medicPlaytime: 119.8,
+    sniperPlaytime: 40.8,
+    spyPlaytime: 66.3
+  };
+};
