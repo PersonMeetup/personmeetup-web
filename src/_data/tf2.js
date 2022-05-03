8d69ef28753eddcbdb96d0be47f49890711a68ba
Leslie Swan <42787368+PersonMeetup@users.noreply.github.com>
2022-05-02 21:39:55 -0600

    Update TF2 hours on Competitive page during build
    Call the Steam API to get up to date playtime data for Team Fortress 2.
    Total playtime is currently being used by templates, further work is
    needed for individual classes.

--- src/_data/tf2.js
+++ src/_data/tf2.js
@@ -1,14 +1,40 @@
-module.exports = function () {
-  return {
-    totalPlaytime: 2951.5,
-    scoutPlaytime: 312,
-    soldierPlaytime: 112.1,
-    pyroPlaytime: 47.7,
-    demomanPlaytime: 52,
-    heavyweaponsPlaytime: 44.4,
-    engineerPlaytime: 397.4,
-    medicPlaytime: 119.8,
-    sniperPlaytime: 40.8,
-    spyPlaytime: 66.3
+const axios = require("axios");
+require("dotenv").config();
+
+module.exports = async function getUser() {
+  var hours = {
+    total: 0,
+    scout: 0,
+    soldier: 0,
+    pyro: 0,
+    demoman: 0,
+    heavyweapons: 0,
+    engineer: 0,
+    medice: 0,
+    sniper: 0,
+    spy: 0
+  }
+
+  try {
+    // Get playtime for Team Fortress 2
+    const overall = await axios.get(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${process.env.STEAM_API_KEY}&steamid=76561198121851448&include_played_free_games=true&format=json`);
+    overall.data.response.games.forEach(game => {
+      if (game.appid == 440) {
+        hours.total = game.playtime_forever / 60;
+      }
+    });
+
+    // Get playtime for individual classes
+    const indiv = await axios.get(`http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=440&key=${process.env.STEAM_API_KEY}&steamid=76561198121851448&format=json`);
+    indiv.data.playerstats.stats.forEach(stat => {
+      if (stat.name.includes(".accum.iPlayTime")) {
+        // Apply each classes playtime to the `hours` object
+        console.log(stat)
+      };
+    });
+  } catch (error) {
+    console.error(error);
   };
+
+  return hours
 };
