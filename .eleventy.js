01fcdf7d6a697664845ca803d7092bb41608997a
Leslie Swan <42787368+PersonMeetup@users.noreply.github.com>
2023-02-16 14:08:41 -0700

    Cleaned up post date formatting

--- .eleventy.js
+++ .eleventy.js
@@ -83,7 +83,7 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
 		// Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
 		return DateTime.fromJSDate(dateObj, { zone: zone || "mst" }).toFormat(
-			format || "dd LLLL yyyy"
+			format || "DD"
 		);
 	});
 
