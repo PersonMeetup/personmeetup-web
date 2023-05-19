9c9bf1811147b85eb865cbc574c7ae9153bf282f
Leslie Swan <contact@personmeetup.ca>
2023-05-18 20:39:55 -0600

    Added layout file to blog directory data

--- src/blog/blog.11tydata.js
+++ src/blog/blog.11tydata.js
@@ -2,6 +2,7 @@ const gitLastUpdated = require("@11ty/eleventy/src/Util/DateGitLastUpdated");
 
 module.exports = {
 	tags: ["blog", "archive"],
+	layout: "layouts/post.njk",
 	date: "git Created",
 	eleventyComputed: {
 		lastmod: ({ page }) => gitLastUpdated(page.inputPath),
