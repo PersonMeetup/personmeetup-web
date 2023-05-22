879e7bba4de638325cef65db0166e3bb7756b64c
Leslie Swan <contact@personmeetup.ca>
2023-05-21 18:17:57 -0600

    Fixed missing library entries

--- src/src.11tydata.js
+++ src/src.11tydata.js
@@ -13,9 +13,9 @@ module.exports = {
 			return data.permalink;
 		},
 		eleventyExcludeFromCollections(data) {
-			// If the page is in `draft:true` mode, or has `permalink:false` exclude
-			// it from any collections since it shouldn't be visible anywhere.
-			if (data.draft || data.permalink === false) {
+			// If the page is in `draft:true` mode, exclude it from any collections
+			// since it shouldn't be visible anywhere.
+			if (data.draft) {
 				return true;
 			}
 			return data.eleventyExcludeFromCollections;
